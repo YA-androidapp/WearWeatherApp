75d3b0751fe84c12d0bd2740cec1ff9fc92d5f4b
YA <YA-androidapp@users.noreply.github.com>
2018-07-01 15:45:53 +0900

    jma

--- app/src/main/java/jp/gr/java_conf/ya/wearweatherapp/WeatherActivity.java
+++ app/src/main/java/jp/gr/java_conf/ya/wearweatherapp/WeatherActivity.java
@@ -3,13 +3,18 @@ package jp.gr.java_conf.ya.wearweatherapp; //  Copyright (c) 2018 YA<ya.androida
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WeatherActivity extends WearableActivity {
 
-    private String uri = "https://jsoup.org/";
+    private String URI_SITE = "jma";
+    private String URI_ID = "319";
+    private String URI = "https://www."+URI_SITE+".go.jp/jp/week/"+URI_ID+".html";
     private TextView mTextView;
 
     @Override
@@ -31,7 +36,11 @@ public class WeatherActivity extends WearableActivity {
         new Thread(new Runnable() {
             @Override
             public final void run() {
-                final Document doc = NetworkUtil.getDocument(uri);
+                final Document doc = NetworkUtil.getDocument(URI);
+//                Elements elements = doc.select("table#infotablefont tr td");
+//                for (Element element : elements) {
+//                    Log.v("WWA",element.outerHtml());
+//                }
                 if (doc != null) {
                     handler.post(new Runnable() {
                         @Override
